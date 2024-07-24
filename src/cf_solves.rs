8bb4dc7c58196ed7f15e077ebf5939a002b94029
vlad <mlprimitif@gmail.com>
2024-07-24 08:32:39 +0700

    add more solution

--- src/cf_solves.rs
+++ src/cf_solves.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io};
+use std::{collections::HashMap, io::{self, Read}};
 pub fn cf_watermelon() {
 
     let mut input = String::new();
@@ -21,7 +21,7 @@ pub fn cf_word_capitalization() {
         .expect("failed to read line");
 
     let first_char = input.chars().next().unwrap().to_uppercase().to_string();
-    
+
     println!("{}{}", first_char, input.chars().skip(1).collect::<String>() )
 }
 
@@ -31,7 +31,7 @@ pub fn cf_boy_or_girl() {
     let mut answer : i32 = 0; // because the null probably, i dont know
 
     io::stdin().read_line(&mut input).expect("Failed to read the line");
-    
+
     for c in input.chars() {
         if !hm.contains_key(&c) {
             hm.insert(c, 1);    
@@ -40,7 +40,7 @@ pub fn cf_boy_or_girl() {
     }
 
     if answer % 2 == 0 {
-        
+
         println!("{}", "CHAT WITH HER!");
     }
     else {
@@ -52,6 +52,82 @@ pub fn cf_boy_or_girl() {
 
 pub fn bear_and_big_brother() {
     let mut input = String::new();
+    let mut year: i32 = 0; 
     io::stdin().read_line(&mut input).expect("failed bng");
 
+    let parts: Vec<&str> = input.split_whitespace().collect(); 
+
+    let mut a = parts[0].parse::<i32>().expect("failed to parse");
+    let mut b = parts[1].parse::<i32>().expect("failed to parse");
+
+    while a <= b{
+        a *=3;
+        b *=2;
+        year += 1;
+    }
+
+    println!("{}", year);
 }
+
+pub fn stones_on_the_table() {
+    let mut input = String::new();
+    let mut stones = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin().read_line(&mut stones).expect("Failed to read line");
+ 
+    let mut answer: i32 = 0;
+    for indx in 0..stones.len() - 1 {
+        if stones.chars().nth(indx) == stones.chars().nth(indx + 1) {
+            answer +=1;
+        }
+    }
+    println!("{}", answer)
+}
+
+pub fn dp_stones_on_the_table() {
+    let mut total = String::new();
+    let mut stones_str = String::new();
+    
+    io::stdin().read_line(&mut total).expect("cannot read line");
+    io::stdin().read_line(&mut stones_str).expect("cannot read line");
+
+    let stones = stones_str.trim().as_bytes();
+    let mut dp = vec![0; stones.len()];
+    for indx in 1..stones.len(){
+        dp[indx] =  dp[indx - 1];
+        if stones[indx] == stones[indx-1] {
+            dp[indx] += 1;
+        }
+    }
+
+    println!("{:?}", dp[stones.len() - 1]);
+}
+
+
+pub fn dp_elephant() {
+    
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).expect("Faild to read line");
+    
+    let mut x: usize = input.trim().parse::<usize>().unwrap();
+
+    let mut dp: Vec<usize> = Vec::new();
+    let steps= vec![5, 4, 3, 2, 1];
+    let mut trace: usize = 1;
+    dp.push(0);
+    let mut x_int: i32 = x as i32;
+    while  x_int > 0 {
+        for step in steps.iter(){
+            x_int = x_int - step;
+            if x_int > 0 {
+                dp.push(dp[trace - 1] + *step as usize);
+                trace += 1;
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+    println!("{}", dp.len());
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,30 @@ mod cf_solves;
 use std::io::Read;
 use std::{collections::HashMap, io};
 use std::{f64::consts::LN_2};
+
 fn main() {
-        let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).expect("Faild to read line");
+    
+    let mut x: usize = input.trim().parse::<usize>().unwrap();
 
-    let mut numbers = input.trim().split_whitespace();
-    let a: f64 = numbers.next().unwrap().parse().unwrap();
-    let b: f64 = numbers.next().unwrap().parse().unwrap();
-
-    // Calculate the minimum y such that Limak's weight exceeds Bob's weight
-    let y = ((b.log(10.0) - a.log(10.0)) / (LN_2 - LN_2)).ceil() as i32;
-
-    println!("{}", y);
+    let mut dp: Vec<usize> = Vec::new();
+    let steps= vec![5, 4, 3, 2, 1];
+    let mut trace: usize = 1;
+    dp.push(0);
+    let mut x_int: i32 = x as i32;
+    while  x_int > 0 {
+        for step in steps.iter(){
+            x_int = x_int - step;
+            if x_int > 0 {
+                dp.push(dp[trace - 1] + *step as usize);
+                trace += 1;
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+    println!("{:?}", dp);
+    println!("{}", dp.len());
 }
